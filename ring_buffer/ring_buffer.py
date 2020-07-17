a6970bc7dc16a618418d7074ce84bd95fd227315
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-07-17 10:58:02 -0700

    work on solving ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None for i in range(capacity)]
+        self.tail = 0
 
     def append(self, item):
-        pass
+        # if there is no capacity
+        if self.capacity == 0:
+            return
 
+        # otherwise add to storage
+        self.storage.append(item)
+
+        # increment self.tail somewhere
+        if len(self.storage) == self.capacity:
+            self.storage.pop(self.tail)
+            self.storage.insert(self.tail, item)
+            self.tail = (self.tail + 1) % self.capacity
+        
+        
+        
     def get(self):
-        pass
+        true_values = [i for i in self.storage if i != None ]
+        return true_values
